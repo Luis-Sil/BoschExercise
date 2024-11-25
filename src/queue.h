c396a75f1dc97c7ec66e6903339d72f462b098dc
Luis Silva <luis_silv@live.com>
2024-11-25 23:45:34 +0000

    Queue and respective tests.

---
+++ src/queue.h
@@ -0,0 +1,195 @@
+#ifndef __QUEUE_H__
+#define __QUEUE_H__
+
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
+
+/**
+ * @brief A thread-safe queue class.
+ * 
+ * This class implements a multi-threaded queue designed for concurrent
+ * writing and reading operations.
+ * 
+ * @tparam T The type of elements stored in the queue.
+ */
+template <typename T>
+class Queue
+{
+public:
+    Queue() = delete; ///< Deleted default constructor to enforce size specification.
+
+    /**
+     * @brief Constructs a queue with a specified capacity.
+     * 
+     * Allocates memory for the queue with the given capacity.
+     * 
+     * @param size The maximum number of elements that the queue can hold.
+     */
+    Queue(int size) : m_filled(), m_data(nullptr), m_capacity(size)
+    {
+        // allocate without constructing
+        m_data = static_cast<T *>(operator new(size * sizeof(T)));
+    }
+
+    /**
+     * @brief Copy constructor.
+     * 
+     * Creates a copy of the specified Queue object.
+     * 
+     * @param src The Queue object to copy from.
+     */
+    Queue(const Queue &src) : m_filled(src.m_filled), m_data(nullptr), m_capacity(size)
+    {
+        m_data = static_cast<T *>(operator new(size * sizeof(T)));
+        for (int i = 1; i < m_filled; i++)
+            new (m_data + i) T(*(src.m_data + i));
+    }
+
+    /**
+     * @brief Destructor.
+     * 
+     * Destroys the queue and its elements, releasing allocated memory.
+     */
+    ~Queue()
+    {
+        // destroy objects that exist
+        for (int i = 0; i < m_filled; i++)
+            (m_data + i)->~T();
+
+        // free all space
+        operator delete(m_data);
+
+        cv.notify_all();
+    }
+
+    /**
+     * @brief Adds a new element to the queue.
+     * 
+     * Inserts a new element into the queue. If the queue is full, the oldest
+     * element is removed to make room for the new element.
+     * 
+     * @param element The element to add to the queue.
+     */
+    void push(const T& element)
+    {
+        std::unique_lock<std::mutex> lck(mtx);
+        if (m_filled < m_capacity)
+        {
+            new (m_data + m_filled) T(element);
+            m_filled += 1;
+        }
+        else
+        {
+            // the queue is full, so we copy all elements to the previous
+            // position
+            for (int i = 1; i < m_filled; i++)
+                *(m_data + i - 1) = *(m_data + i);
+
+            *(m_data + m_filled - 1) = element;
+        }
+
+        // a new element is added, so notify the reader thread
+        cv.notify_one();
+    }
+
+    /**
+     * @brief Removes and returns the oldest element in the queue.
+     * 
+     * Waits indefinitely until an element is available in the queue, then
+     * removes and returns the oldest element.
+     * 
+     * @return The oldest element in the queue.
+     */
+    T pop()
+    {
+        // get stuck while there's no new elements
+        std::unique_lock<std::mutex> lck(mtx);
+        cv.wait(lck, [this]()
+                { return m_filled != 0; });
+        
+        // take 1st element and copy all the others to the previous position
+        T popped = *(m_data);
+        for (int i = 1; i < m_filled; i++)
+            *(m_data + i - 1) = *(m_data + i);
+
+        // clear the last one and decrease the number of elements in the queue
+        (m_data + m_filled)->~T();
+        m_filled -= 1;
+
+        return popped;
+    }
+
+    /**
+     * @brief Removes and returns the oldest element in the queue, with a timeout.
+     * 
+     * Waits for a specified period for an element to be available in the queue.
+     * If the timeout period elapses without an element being available, an
+     * exception is thrown.
+     * 
+     * @param milliseconds_val The timeout period in milliseconds.
+     * 
+     * @return The oldest element in the queue.
+     * 
+     * @throws std::system_error If the timeout period elapses.
+     */
+    T popWithTimeout(int milliseconds_val)
+    {
+        // get stuck while there's no elements
+        std::unique_lock<std::mutex> lck(mtx);
+        bool not_empty = cv.wait_for(lck, std::chrono::milliseconds(milliseconds_val), [this]()
+                                     { return m_filled != 0; });
+
+        // queue is still empty and lock was freed.
+        if (!not_empty)
+            throw std::system_error{std::make_error_code(std::errc::operation_would_block),
+                                    "Queue: pop() timeout"};
+
+        // pop the oldest element.
+        T popped = *(m_data);
+        for (int i = 1; i < m_filled; i++)
+            *(m_data + i - 1) = *(m_data + i);
+
+        (m_data + m_filled)->~T();
+        m_filled -= 1;
+
+        return popped;
+    }
+
+    /**
+     * @brief Number of Queue elements getter.
+     * 
+     * Returns the current number of elements in the Queue.
+     * 
+     * @return int Number of element in the Queue.
+     */
+    int count() const { return m_filled; } // Amount of elements stored now
+
+    /**
+     * @brief Queue capacity getter.
+     * 
+     * Returns the capacity of the queue.
+     * 
+     * @return int Capacity of the queue.
+     */
+    int size() const { return m_capacity; }  // Max number of elements
+
+    /**
+     * @brief Gets the data pointer of the queue elements.
+     * 
+     * Returns a constant pointer to the queue elements.
+     * 
+     * @return const T* Constant pointer to the queue elements.
+     */
+    const T *data() const { return m_data; }
+
+private:
+    T *m_data;                    /**< Pointer to the queue elements */
+    int m_filled;                 /**< Current number of elements in the queue */
+    int m_capacity;               /**< Maximum capacity of the queue */
+
+    std::mutex mtx{};             /**< Mutex for thread safety */
+    std::condition_variable cv{}; /**< Condition variable for synchronization */
+};
+
+#endif
---
+++ test/test.cpp
@@ -0,0 +1,143 @@
+#include "queue.h"
+#include <catch2/catch_test_macros.hpp>
+
+#include <vector>
+#include <iostream>
+#include <chrono>
+#include <atomic>
+
+using namespace std::chrono;
+
+TEST_CASE("Construct Queue")
+{
+    Queue<int> queue(5);
+    REQUIRE(queue.count() == 0);
+    REQUIRE(queue.size() == 5);
+}
+
+TEST_CASE("Push to Queue: not full")
+{
+    Queue<int> queue(3);
+
+    std::vector<int> expected_data{1, 2, 3};
+    for (auto element : expected_data)
+        queue.push(element);
+
+    REQUIRE(queue.count() == queue.size());
+
+    std::vector<int> obtained_data{};
+    for (int i = 0; i < queue.count(); i++)
+        obtained_data.push_back(*(queue.data() + i));
+
+    REQUIRE(obtained_data == expected_data);
+}
+
+TEST_CASE("Push to Queue: full")
+{
+    Queue<int> queue(3);
+    for (auto element : {1, 2, 3})
+        queue.push(element);
+
+    queue.push(10);
+
+    std::vector<int> expected_data{2, 3, 10};
+    std::vector<int> obtained_data{};
+
+    for (int i = 0; i < queue.count(); i++)
+        obtained_data.push_back(*(queue.data() + i));
+
+    REQUIRE(obtained_data == expected_data);
+
+    queue.push(25);
+    queue.push(33);
+
+    expected_data = {10, 25, 33};
+    obtained_data.clear();
+
+    for (int i = 0; i < queue.count(); i++)
+        obtained_data.push_back(*(queue.data() + i));
+
+    REQUIRE(obtained_data == expected_data);
+}
+
+TEST_CASE("Pop from Queue (using pop method): without timeout")
+{
+    Queue<int> queue(5);
+    for (auto element : {1, 3, 2, 6})
+        queue.push(element);
+
+    REQUIRE(queue.count() == 4);
+
+    int popped = queue.pop();
+    std::vector<int> obtained_data{};
+    for (int i = 0; i < queue.count(); i++)
+        obtained_data.push_back(*(queue.data() + i));
+
+    REQUIRE(popped == 1);
+    REQUIRE(queue.count() == 3);
+    REQUIRE(obtained_data == std::vector<int>{3, 2, 6});
+
+    popped = queue.pop();
+    obtained_data.clear();
+    for (int i = 0; i < queue.count(); i++)
+        obtained_data.push_back(*(queue.data() + i));
+
+    REQUIRE(popped == 3);
+    REQUIRE(queue.count() == 2);
+    REQUIRE(obtained_data == std::vector<int>{2, 6});
+}
+
+template <typename T>
+void read(Queue<T> &queue, std::vector<T> &elements)
+{
+    elements.push_back(queue.pop()); // pops 1
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    elements.push_back(queue.pop()); // pops 3
+    elements.push_back(queue.pop()); // pops 4
+    elements.push_back(queue.pop()); // blocks
+    // -> 5 is added and this is released.
+}
+
+TEST_CASE("Write and Read from Queue, concurrently")
+{
+    Queue<int> queue(2);
+    std::vector<int> elements;
+
+    std::thread reader(read<int>, std::ref(queue), std::ref(elements));
+
+    std::thread writer([&queue]()
+                       {
+                        queue.push(1);
+                        std::this_thread::sleep_for(std::chrono::seconds(1));
+                        queue.push(2);
+                        queue.push(3);
+                        queue.push(4);
+                        std::this_thread::sleep_for(std::chrono::seconds(5));
+                        queue.push(5);
+                        });
+    reader.join();
+    writer.join();
+
+    REQUIRE(elements == std::vector<int>{1, 3, 4, 5});
+    REQUIRE(queue.count() == 0); 
+}
+
+void thisThrows()
+{
+    Queue<int> queue(2);
+    queue.popWithTimeout(100);
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+}
+
+TEST_CASE("popWithTimeout throws exception")
+{
+    try
+    {
+        thisThrows();
+        CHECK(false);
+    }
+    catch (const std::exception &e)
+    {
+        CHECK(true);
+    }
+}
